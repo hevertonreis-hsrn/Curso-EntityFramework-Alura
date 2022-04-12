2c701a884c96b7d2200f2e19d61bec2d7c9eb34b
Heverton Reis <heverton.reis04@gmail.com>
2022-04-12 16:28:53 -0300

    doc: Migrations, Relacionamento 1:1/1:N/N:N, SELECT e JOIN com Entity

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,29 +1,170 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
-    class Program2
+    class Program
     {
         static void Main(string[] args)
         {
-            var paoFrances = new Produto();
-            paoFrances.Nome = "Pão Francês";
-            paoFrances.PrecoUnitario = 0.40;
-            paoFrances.Unidade = "Unidade";
-            paoFrances.Categoria = "Padaria";
 
-            var compra = new Compra();
-            compra.Quantidade = 6;
-            compra.Produto = paoFrances;
-            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+            using (var contexto2 = new LojaContext())
+            {
+                var serviceProvider = contexto2.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var promocao = contexto2
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+
+                Console.WriteLine("\nMostrando os produtos da promoção...");
+                foreach (var item in promocao.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
+            }
+
+
+        }
+
+        private static void IncluirPromocao()
+        {
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var promocao = new Promocao();
+                promocao.Descricao = "Queima Total Janeiro 2017";
+                promocao.DataInicio = new DateTime(2017, 1, 1);
+                promocao.DataTermino = new DateTime(2017, 1, 31);
+
+                var produtos = contexto
+                    .Produtos
+                    .Where(p => p.Categoria == "Bebidas")
+                    .ToList();
+
+                foreach (var item in produtos)
+                {
+                    promocao.IncluiProduto(item);
+                }
+
+                contexto.Promocoes.Add(promocao);
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+
+            }
+
+        }
+
+        private static void UmParaUm()
+        {
+            var fulano = new Cliente();
+            fulano.Nome = "Fulaninho de Tal";
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = 12,
+                Logradouro = "Rua dos Inválidos",
+                Complemento = "Sobrado",
+                Bairro = "Centro",
+                Cidade = "Central City"
+            };
+
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                contexto.Clientes.Add(fulano);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void MuitosParaMuitos()
+        {
+            var p1 = new Produto()
+            {
+                Nome = "Suco de Uva",
+                Categoria = "Bebidas",
+                PrecoUnitario = 8.79,
+                Unidade = "Litros"
+            };
+            var p2 = new Produto()
+            {
+                Nome = "Café",
+                Categoria = "Bebidas",
+                PrecoUnitario = 12.45,
+                Unidade = "Gramas"
+            };
+            var p3 = new Produto()
+            {
+                Nome = "Ovo de Páscoa - GSW",
+                Categoria = "Chocolates",
+                PrecoUnitario = 79.90,
+                Unidade = "Gramas"
+            };
+
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Páscoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddDays(8);
+
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
+
+            //var paoFrances = new Produto();
+            //paoFrances.Nome = "Pão Francês";
+            //paoFrances.PrecoUnitario = 0.40;
+            //paoFrances.Unidade = "Unidade";
+            //paoFrances.Categoria = "Padaria";
+
+            //var compra = new Compra();
+            //compra.Quantidade = 6;
+            //compra.Produto = paoFrances;
+            //compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                //contexto.Compras.Add(compra);
+
+                //contexto.Promocoes.Add(promocaoDePascoa);
+                //ExibeEntries(contexto.ChangeTracker.Entries());
+                //contexto.SaveChanges();
+
+                var promocao = contexto.Promocoes.Find(1);
+                contexto.Promocoes.Remove(promocao);
+
+                //ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("=================");
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
         }
     }
 }
---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/obj/Debug/yoqqvttm.sql
@@ -0,0 +1,59 @@
+﻿IF OBJECT_ID(N'__EFMigrationsHistory') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Produtos] (
+    [Id] int NOT NULL IDENTITY,
+    [Categoria] nvarchar(max),
+    [Nome] nvarchar(max),
+    [Preco] float NOT NULL,
+    CONSTRAINT [PK_Produtos] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220411184257_Inicial', N'1.1.2');
+
+GO
+
+EXEC sp_rename N'Produtos.Preco', N'PrecoUnitario', N'COLUMN';
+
+GO
+
+ALTER TABLE [Produtos] ADD [Unidade] nvarchar(max);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220411184516_Unidade', N'1.1.2');
+
+GO
+
+CREATE TABLE [Compras] (
+    [Id] int NOT NULL IDENTITY,
+    [Preco] float NOT NULL,
+    [ProdutoId] int NOT NULL,
+    [Quantidade] int NOT NULL,
+    CONSTRAINT [PK_Compras] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Compras_Produtos_ProdutoId] FOREIGN KEY ([ProdutoId]) REFERENCES [Produtos] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_Compras_ProdutoId] ON [Compras] ([ProdutoId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220411191923_Compra', N'1.1.2');
+
+GO
+
